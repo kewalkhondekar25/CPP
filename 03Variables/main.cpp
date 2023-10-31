b6b94f55b59d2711557470558325e3a3427fd5f2
keal khondekar <kewalkhondekar25@gmail.com>
2023-11-01 01:52:50 +0530

    feat: added variables

---
+++ 03Variables/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    //variables
+    int _k = 25;
+    int marksInMaths = 99;
+
+    //camelCase Notation
+    cout<<_k<<endl;
+    cout<<marksInMaths;
+}
