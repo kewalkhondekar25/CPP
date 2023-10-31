9992b8d96eb739002ec36f9160759fdc78f281e3
keal khondekar <kewalkhondekar25@gmail.com>
2023-11-01 02:06:27 +0530

    feat: added user inputs

---
+++ 04Inputs/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    string name;
+    int age;
+
+    cout<<"Enter your name"<<endl;
+    cin>>name;
+
+    cout<<"Enter your age"<<endl;
+    cin>>age;
+
+    cout<<"Hie!, Your name is: "<<name<<endl;
+    cout<<"Your age is: "<<age;
+
+    return 0;
+}
