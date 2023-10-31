0858c399892841285ca185ba1a414155299efa34
keal khondekar <kewalkhondekar25@gmail.com>
2023-11-01 03:15:00 +0530

    feat: added if-else && switch conditionals

--- 06Conditionals/main.cpp
+++ 06Conditionals/main.cpp
@@ -8,13 +8,41 @@ int main(){
     cin>>age;
 
     //if-else conditionals
-    if(age > 120 || 5){
-        cout<<"Invalid age";
+    if(age > 120 ){
+        cout<<"Invalid age"<<endl;
     }else if(age >= 18){
-        cout<<"You are eligible to vote";
+        cout<<"You are eligible to vote"<<endl;
     }else{
-        cout<<"You are not eligible to vote";
+        cout<<"You are not eligible to vote"<<endl;
     }
+
+    int number;
+    cout<<"Enter a number between 1 to 5: "<<endl;
+    cin>>number;
+
+    //switch conditionals
+    switch (number)
+    {
+    case 1:
+        cout<<"1 is Odd number";
+        break;
+    case 2:
+        cout<<"1 is Odd number";
+        break;        
+    case 3:
+        cout<<"1 is Odd number";
+        break;
+    case 4:
+        cout<<"1 is Odd number";
+        break;
+    case 5:
+        cout<<"1 is Odd number";
+        break;                
+    default:
+        cout<<"Please enter number between 1 and 5 only:)";
+        break;
+    }
+
 
     return 0;
 
