fe22b63c8ac12bf84408d89e72b0cb38dbeac7fb
keal khondekar <kewalkhondekar25@gmail.com>
2023-11-01 02:23:33 +0530

    feat: added operators

---
+++ 05Operators/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a, b;
+
+    cout<<"Enter first number: "<<endl;
+    cin>>a;
+    cout<<"Enter second number: "<<endl;
+    cin>>b;
+
+    //arithmaetic operators
+    cout<<"a + b is: "<<a + b<<endl;
+    cout<<"a - b is: "<<a - b<<endl;
+    cout<<"a * b is: "<<a * b<<endl;
+    //typeCasting
+    cout<<"a / b is: "<<(float) a / b<<endl;
+
+    return 0;
+
+}
