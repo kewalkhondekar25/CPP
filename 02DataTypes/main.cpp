840c2aa3598fec7598ab1994a8cfac30c496d92e
keal khondekar <kewalkhondekar25@gmail.com>
2023-11-01 01:32:19 +0530

    feat: added data types

---
+++ 02DataTypes/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    short k = 9;
+    int a;
+    long b;
+    long long c;
+    float score1 = 99.98;
+    double score2 = 23.987;
+    long double  score3 = 23.123;
+
+    a = 5;
+
+
+    string name = "kewal";
+
+    cout<<k<<endl;
+    cout<<a<<endl;
+    cout<<name<<endl;
+    cout<<"the score is "<<score3;
+
+    return 0;
+
+}
