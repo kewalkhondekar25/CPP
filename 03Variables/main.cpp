2b54c2a092e86fd09ae9c1b946bb7e641331c50a
keal khondekar <kewalkhondekar25@gmail.com>
2023-11-07 23:58:39 +0530

    feat: added scope variables

--- 03Variables/main.cpp
+++ 03Variables/main.cpp
@@ -6,8 +6,16 @@ int main(){
     //variables
     int _k = 25;
     int marksInMaths = 99;
+    // \n escape sequence character
 
     //camelCase Notation
     cout<<_k<<endl;
     cout<<marksInMaths;
+
+    //variable scope
+    //local variable
+    int local = 5;
+    cout<<"Local variable is: "<<local;
+
+
 }
